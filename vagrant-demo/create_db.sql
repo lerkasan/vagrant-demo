e9b1bae567699cc86fabf5aacc28a98582126d12
lerkasan <lerkasan@gmail.com>
2016-10-16 14:13:56 +0300

    initial commit

---
+++ vagrant-demo/create_db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE db_webapp;
+CREATE USER db_user WITH password 'password';
+GRANT ALL privileges ON DATABASE db_webapp TO db_user;
---
+++ vagrant-dev/vagrant_dev_startup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get -qq -y --force-yes install oracle-java8-set-default
+
+echo 'JAVA_HOME="/usr/lib/jvm/java-8-oracle"' >> /etc/environment
+source /etc/environment
+
+sudo apt-get -qq -y --force-yes install \
+mc \
+git \
+maven \
+postgresql
+
+echo 'M2_HOME="/usr/share/maven"' >> /etc/environment
+source /etc/environment
+
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get -qq -y --force-yes install \
+nodejs \
+build-essential
+
+sudo npm install gulp -g
+sudo npm install bower -g
+
+sudo echo '<?xml version="1.0" encoding="UTF-8"?>
+  <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" 
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+    xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
+  <pluginGroups>
+    <pluginGroup>org.eclipse.jetty</pluginGroup>
+  </pluginGroups>
+</settings>' > /usr/share/maven/conf/settings.xml
+
+echo "*********** INSTALLATION FINISHED ***********."
+
+mvn jetty:run
